4f4cad54a7fb8a5bc385960f8112b486b6c38593
Suin <tktw555@gmail.com>
2023-07-13 15:22:20 +0900

    오류 수정

--- src/main/java/com/sparta/likeblog/controller/LikeController.java
+++ src/main/java/com/sparta/likeblog/controller/LikeController.java
@@ -17,22 +17,22 @@ public class LikeController {
 
     private final LikeService likeService;
 
-    @PostMapping("/{postId}")
+    @PostMapping("/post/{postId}")
     public PostResponseDto addLike(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long postId) {
         return likeService.addLike(userDetails.getUser(), postId);
     }
 
-    @DeleteMapping("/{postId}")
+    @DeleteMapping("/post/{postId}")
     public PostResponseDto cancelLike(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long postId) {
         return likeService.cancelLike(userDetails.getUser(), postId);
     }
 
-    @PostMapping("/{postId}/{commentId}")
+    @PostMapping("post/{postId}/comment/{commentId}")
     public CommentResponseDto addCommentLike(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long postId, @PathVariable Long commentId) {
         return likeService.commentAddLike(userDetails.getUser(), postId, commentId);
     }
 
-    @DeleteMapping("/{postId}/{commentId}")
+    @DeleteMapping("post/{postId}/comment/{commentId}")
     public CommentResponseDto cancelCommentLike(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long postId, @PathVariable Long commentId) {
         return likeService.commentCancelLike(userDetails.getUser(), postId, commentId);
     }
